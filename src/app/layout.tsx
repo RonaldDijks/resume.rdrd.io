dfa0bc3181e4171d77614d17b3280aa86d1182db
Ronald Dijks <ronald@rdrd.io>
2025-01-31 19:00:35 +0100

    fix injecting css variables for geist and geist mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground font-sans sm:p-12`}
+        className={`antialiased bg-background text-foreground font-sans sm:p-12`}
       >
         {children}
       </body>
