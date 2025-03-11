df7424c73dedf86bdfbc50f28604cc6c131ea51a
Ronald Dijks <ronald@rdrd.io>
2025-03-11 01:29:25 +0100

    @/components/work: fix margins

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -67,9 +67,9 @@ export interface ExperienceProps {
 
 const Experience: React.FC<ExperienceProps> = (props) => {
   return (
-    <div className="grid gap-8 grid-cols-[1fr_2fr] text-sm text-muted hover:bg-zinc-800 py-3 px-2 -mx-2">
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-[1fr_2fr] text-sm text-muted hover:bg-zinc-800 py-3 px-2 -mx-2">
       <div>
-        <h3 className="text-foreground  [text-wrap:balance]">
+        <h3 className="text-foreground [text-wrap:balance] mb-0 md:mb-2">
           <Link href={props.company.url}>
             {props.position} at {props.company.name}
           </Link>
@@ -79,11 +79,13 @@ const Experience: React.FC<ExperienceProps> = (props) => {
         </p>
         <p>{props.location}</p>
       </div>
-      <div className="grid gap-2">
-        {props.details.map((detail) => (
-          <div key={detail}>{detail}</div>
-        ))}
-      </div>
+      {props.details.length > 0 && (
+        <div className="grid gap-2">
+          {props.details.map((detail) => (
+            <div key={detail}>{detail}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
