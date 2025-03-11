1427360ca817e027c21a70786ccbe12c09269637
Ronald Dijks <ronald@rdrd.io>
2025-03-11 01:21:30 +0100

    @/components/Logo: make scaling smaller on hover

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <div className="mb-4 flex animate-fade-in-up">
       <NextLink
         href="/"
-        className="block font-mono font-bold text-5xl transition-transform ease-in-out duration-200 hover:scale-110"
+        className="block font-mono font-bold text-5xl transition-transform ease-in-out duration-200 hover:scale-105"
       >
         <span>rd</span>
         <span className="text-zinc-400">rd</span>
